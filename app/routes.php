57341dace40b278f147755c1738b14a0f9fea91e
Boyan Yordanov <specterbg@gmail.com>
2014-01-13 22:04:39 +0200

    Added information about the planned api routes.

--- app/routes.php
+++ app/routes.php
@@ -5,36 +5,97 @@
 | Application Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
+| The come route and a route to catch all non-api routes 
+| and display the home template,
+| so that angular can do the routing on the frontend.
 |
 */
 
-Route::get('/{userId}', function($userId) {
-  $user = \User::find($userId);
+Route::get('/', ['as' => 'home', 'uses' => 'HomeController@getIndex']);
+Route::get('{any?}', 'HomeController@getIndex');
 
-  $folders = [];
+/*
+|--------------------------------------------------------------------------
+| User Resource
+|--------------------------------------------------------------------------
+| 
+| The users of the application. Will serve for CRUD operations.
+|
+| /api/v1/users   - GET, POST
+| /api/v1/users/{id} - GET, POST, PUT, DELETE 
+|
+*/
 
-  foreach ($user->folders as $folder) {
-    $folders[] = [
-      'name' => $folder->name,
-      'slug' => $folder->slug,
-      'bookmarks' => array_pluck($folder->bookmarks->toArray(), 'url')
-    ];
-  }
+/*
+|--------------------------------------------------------------------------
+| Session Resource
+|--------------------------------------------------------------------------
+| 
+| Responsible for loggin a user in and out and retrieving information about
+| the session.
+|
+| session - GET     - Returns whether the user is loggin in and information 
+|                     about it
+| session - POST    - Loggs a user in
+| session - DELETE  - Loggs a user out
+|
+*/
 
-  $bookmarks = [];
-  foreach ($user->bookmarks as $bookmark) {
-    $bookmarks [] = [
-      'title' => $bookmark->title,
-      'url'   => $bookmark->url,
-      'tags'  => implode(array_pluck($bookmark->tags->toArray(), 'name'), ', ')
-    ];
-  }
-  return Response::make([
-    'user' => $user->email,
-    'folders' => $folders,
-    'bookmarks' => $bookmarks
-  ]);
-});
+/*
+|--------------------------------------------------------------------------
+| Bookmarks Resource
+|--------------------------------------------------------------------------
+| 
+| The heart resource of the application. This will be responsible for the 
+| bookmarks each users saves.
+| 
+| - Adding or moving a bookmark to a folder should happen with an update, 
+|   using the PUT request.
+|
+| - Adding or removing a tag from the bookmark should happen again with
+|   an update using the PUT request 
+|
+| /api/v1/users/{userId}/bookmarks                      - GET, POSTr    
+| /api/v1/users/{userId}/bookmarks/{bookmarkId}         - GET, POST, PUT, DELETE
+|
+| URL parameters:
+| - search - search term to filter the bookmark 
+| - folder - filters the bookmarks based on folder
+| - tags   - filters the bookmarks based on tags
+|
+| Examples:
+| /api/v1/users/{userId}/bookmarks?filter="development"
+| /api/v1/users/{userId}/bookmarks?folder=2
+| /api/v1/users/{userId}/bookmarks?tags=1,2,3
+| /api/v1/users/{userId}/bookmarks?filter="kittens"&tags=1,2,3
+| /api/v1/users/{userId}/bookmarks?filter="php"&folder=3
+|
+*/
+
+
+/*
+|--------------------------------------------------------------------------
+| Folder Resource
+|--------------------------------------------------------------------------
+| 
+| The folder resource. Responsible for retrieving, creating, updating and deleting  
+| folders for a user.
+|
+| /api/v1/users/{userId}/folders            - GET, POST
+| /api/v1/users/{userId}/folders/{folderId} - GET, POST, PUT, DELETE 
+|
+*/
+
+/*
+|--------------------------------------------------------------------------
+| Tag Resource
+|--------------------------------------------------------------------------
+| 
+| The tag resource. Responsible for retrieving, creating, updating and deleting  
+| tags for a user.
+|
+| /api/v1/users/{userId}/tags         - GET, POST
+| /api/v1/users/{userId}/tags/{tagId} - GET, POST, PUT, DELETE 
+|
+*/
+
